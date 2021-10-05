a4a2163c7ea5f8210a267fdb706a4dabbec4b516
ThijscS <thijs.suurmond@gmail.com>
2021-10-05 09:55:11 +0200

    Interfaces

--- src/main/java/nl/vu/labs/phoenix/ap/SetInterface.java
+++ src/main/java/nl/vu/labs/phoenix/ap/SetInterface.java
@@ -1,14 +1,22 @@
 package nl.vu.labs.phoenix.ap;
 
-/* [1] Set Specification
- * 	   -- Complete the specification for a set interface.
- * 		  See the List interface for inspiration
- */
 public interface SetInterface<T extends Comparable<T>> {
-	
-	/* 
-	 * [2] Mandatory methods. Make sure you do not modify these!
-	 * 	   -- Complete the specifications of these methods
+
+	/*
+	 * Elements : Objects of type T
+	 * Structure: none
+	 * Domain   : all sets with minimum 0 and no maximum elements
+	 *
+	 * Constructors:
+	 *
+	 * Set();
+	 * PRE  -
+	 * POST - A new Set-object has been made and is the empty Set.
+	 *
+	 * Set (Set src);
+	 * PRE  -
+	 * POST - A new Set-object has been made and contains a copy of the elements src.
+	 *
 	 */
 	
 	/**
@@ -17,30 +25,69 @@ public interface SetInterface<T extends Comparable<T>> {
 	 * 	true  - element was inserted
 	 * 	false - element was already present 
 	 */
-	boolean add(T t);
-	
+
+	boolean add(T element);
+		/* PRE -
+		   POST -   true  - element was inserted
+	  	  			false - element was already present
+		 */
+
 	T get();
-	
+		/*  PRE  - Set is not empty
+	   		POST - Returns a copy of a random element
+		 */
+
 	boolean remove(T t);
-	
+		/*  PRE  -
+  			POST - Element is not in the set
+    	 */
+
 	int size();
-	
+		/*PRE -
+		  POST - the size of the zet is returned
+		 */
+
 	SetInterface<T> copy();
-	
+			/* PRE  -
+			   POST - A new Set-object has been made and contains a copy of the elements src.
+			 */
 	/*
 	 * [3] Methods for set operations 
 	 * 	   -- Add methods to perform the 4 basic set operations 
 	 * 		  (union, intersection, difference, symmetric difference)
 	 */
-	
-	
-	// your code here
-	
+
+	SetInterface<T> intersection(SetInterface<T> set2);
+		/*  PRE  -
+			POST - returns a new set with copies ofThe intersection of set and set2 is returned
+		 */
+
+
+	SetInterface<T> union(SetInterface<T> set2);
+		/*  PRE  -
+			POST - Success: Returns a new set containing copies of all the elements in both sets
+			 	   Failure: the union of set and set2 consists of more elements than MAX_ELEMENTS
+		 */
+
+	SetInterface<T> difference(SetInterface<T> set2);
+		/*  PRE  -
+			POST - Returns a new set containing copies of the elements in set that are not in set2
+		 */
+
+	SetInterface<T> symmetricDifference(SetInterface<T> set2);
+		/*  PRE  -
+			POST - Success: Returns a new set containing copies of the elements in set1 and set2 that are not in their intersection
+			 	 - failure: the symmetricdifference of set and set2 consists of more elements than MAX_ELEMENTS
+		 */
 	
 	/* 
 	 * [4] Add anything else you think belongs to this interface 
 	 */
-	
-	// your code here
+
+	boolean checkForPresence(T element);
+		/* PRE  -
+		   POST - Returns true if element is contained in set
+  		    	- Returns false if element is not contained in set
+		 */
 	
 }
